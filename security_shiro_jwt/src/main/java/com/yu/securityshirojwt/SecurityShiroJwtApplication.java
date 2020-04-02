80de95185298435347777267dfde224cde3dc875
yu <773024110@qq.com>
2020-04-02 14:53:51 +0800

    初始化 shiro模块

---
+++ security_shiro_jwt/src/main/java/com/yu/securityshirojwt/SecurityShiroJwtApplication.java
@@ -0,0 +1,13 @@
+package com.yu.securityshirojwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityShiroJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityShiroJwtApplication.class, args);
+	}
+
+}
---
+++ security_shiro_jwt/src/test/java/com/yu/securityshirojwt/SecurityShiroJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yu.securityshirojwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityShiroJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
